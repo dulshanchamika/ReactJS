19a2066f4f81e4a2c168843c58675fa8a737f8fa
Dulshan Chamika <dulshanchamika7@gmail.com>
2025-03-24 16:17:09 +0530

    update

---
+++ movie_app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import createRoot from ReactDOM
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
